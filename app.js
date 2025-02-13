2a9ef80a83f65d01d4892b0aba76f195cb4ac940
Youssef <youcef.jair@gmail.com>
2025-02-13 12:44:40 +0100

    Correction des fichiers modifiés et gestion des fins de ligne

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error');
-
 const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,6 +16,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404);
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
